8f7301d37455c450475f1ce57e08ef94fb0221b3
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 13:26:07 +0530

    chore(app): configure core providers including router, http client, and authentication interceptor

---
+++ src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import {httpManagerInterceptor} from './interceptor/auth';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([httpManagerInterceptor])
+    ),
+    provideRouter(routes)
+  ]
+};
