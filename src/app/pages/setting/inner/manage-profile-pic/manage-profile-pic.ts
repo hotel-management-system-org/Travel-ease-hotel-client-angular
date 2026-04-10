d34fd488ebf9f1bea893fc8cf41ce579c2d15070
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 14:34:22 +0530

    feat(profile): add ManageProfilePic component scaffold

---
+++ src/app/pages/setting/inner/manage-profile-pic/manage-profile-pic.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-profile-pic',
+  imports: [],
+  templateUrl: './manage-profile-pic.html',
+  styleUrl: './manage-profile-pic.scss'
+})
+export class ManageProfilePic {
+
+}
