fb85d011fdcbdd1b958c6332a11a93597195150a
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 14:44:50 +0530

    feat(auth): add ResetPwd component scaffold

---
+++ src/app/features/security/reset-password/reset-pwd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-reset-pwd',
+  imports: [],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.scss'
+})
+export class ResetPwd {
+
+}
