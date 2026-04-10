b6e798586071fbbc05d852d10c0d8b33b0f594de
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 14:45:34 +0530

    feat(auth): add SecurityContext layout component with router outlet

---
+++ src/app/features/security/security-context/security-context.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-security-context',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './security-context.html',
+  styleUrl: './security-context.scss'
+})
+export class SecurityContext {
+
+}
