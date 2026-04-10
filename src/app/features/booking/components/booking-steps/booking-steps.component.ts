db91ae9262451f7eed2daa9306f169829393b86e
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 13:39:58 +0530

    feat(booking-steps): add reusable checkout progress indicator component

---
+++ src/app/features/booking/components/booking-steps/booking-steps.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-steps',
+  templateUrl: './booking-steps.component.html',
+  styleUrls: ['./booking-steps.component.scss']
+})
+export class BookingStepsComponent {}
