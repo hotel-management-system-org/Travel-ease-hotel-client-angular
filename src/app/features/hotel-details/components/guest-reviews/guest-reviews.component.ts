648f35827f340d4e9a6da0620acdc365fa0f7a91
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 13:59:23 +0530

    feat(ui/reviews): introduce guest reviews component for hotel review section

---
+++ src/app/features/hotel-details/components/guest-reviews/guest-reviews.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-reviews',
+  templateUrl: './guest-reviews.component.html',
+  styleUrls: ['./guest-reviews.component.scss']
+})
+export class GuestReviewsComponent {}
