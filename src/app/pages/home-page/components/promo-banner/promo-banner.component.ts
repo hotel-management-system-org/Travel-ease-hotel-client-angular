c7c4cb8c28af046991fc23d8c7225c897d955c01
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 14:41:39 +0530

    feat(ui): add PromoBannerComponent scaffold

---
+++ src/app/pages/home-page/components/promo-banner/promo-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-banner',
+  templateUrl: './promo-banner.component.html',
+  styleUrls: ['./promo-banner.component.scss']
+})
+export class PromoBannerComponent {}
