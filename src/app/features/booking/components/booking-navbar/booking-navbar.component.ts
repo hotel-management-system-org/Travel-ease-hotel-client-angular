e3f8d9da556b9faf75ba64952920c3232a073f8e
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 13:38:05 +0530

    feat(ui): add booking navbar component as reusable layout header

---
+++ src/app/features/booking/components/booking-navbar/booking-navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-navbar',
+  templateUrl: './booking-navbar.component.html',
+  styleUrls: ['./booking-navbar.component.scss']
+})
+export class BookingNavbarComponent {}
