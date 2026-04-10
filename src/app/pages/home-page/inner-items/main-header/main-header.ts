fdb02a9666c0fd1ea80da9c3047c3faf1688ff4d
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-10 14:32:49 +0530

    feat(layout): add MainHeader component with RouterLink support

---
+++ src/app/pages/home-page/inner-items/main-header/main-header.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-main-header',
+  imports: [
+    RouterLink,
+  ],
+  templateUrl: './main-header.html',
+  styleUrl: './main-header.scss'
+})
+export class MainHeader {
+
+}
