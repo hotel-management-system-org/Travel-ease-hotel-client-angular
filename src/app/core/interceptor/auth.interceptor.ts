2eaa6bba5ce9390518bd915b814c84d83f2684fa
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-07 23:05:49 +0530

    feat(auth): implement HTTP interceptor with JWT attachment and automatic refresh token handling on 401 errors

--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -1,25 +1,55 @@
 import {inject} from '@angular/core';
 import {
-   HttpErrorResponse, HttpInterceptorFn
+  HttpClient,
+  HttpErrorResponse, HttpInterceptorFn
 } from '@angular/common/http';
-import {catchError, throwError} from 'rxjs';
+import {catchError, switchMap, throwError} from 'rxjs';
 import {CookieManagerService} from '../services/cookie/cookie-manager.service';
+import {environment} from '../../../environments/environment';
 
-
-
-  export const httpManagerInterceptor: HttpInterceptorFn = (req, next) => {
+export const httpManagerInterceptor: HttpInterceptorFn = (req, next) => {
   let cookieManager= inject(CookieManagerService);
+  const http = inject(HttpClient)
 
+  const baseUrl = environment.baseUrl+'/user-service/api/users/visitors';
+  const TOKEN_KEY = 'token';
+  const REFRESH_TOKEN_KEY = 'refresh_token';
 
-
-  if (cookieManager.tokenExists('token')) {
-    const token = cookieManager.getToken('token')
+  if (cookieManager.tokenExists(TOKEN_KEY)) {
+    const token = cookieManager.getToken(TOKEN_KEY)
     req = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + token)
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
     });
   }
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
+      if( error.status === 401){
+        const refreshToken = cookieManager.getToken(REFRESH_TOKEN_KEY);
+
+        if (!refreshToken){
+          return throwError(() => error);
+        }
+
+        return http.post<{assessToke: string}>(`${baseUrl}/refresh-token`, {refreshToken}).pipe(
+          switchMap((response) => {
+            cookieManager.set(TOKEN_KEY, response.assessToke);
+
+            const retryReq = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${response.assessToke}`
+              }
+            });
+            return next(retryReq)
+          }),
+          catchError((refreshError) => {
+            cookieManager.clearToken(TOKEN_KEY);
+            cookieManager.clearToken(REFRESH_TOKEN_KEY);
+            return throwError(() => refreshError)
+          })
+        );
+      }
       return throwError(()=>error);
     })
   );
